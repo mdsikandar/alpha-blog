7e06fd7c3e3016351f0ce1f0cf10e371de80b3f2
Md Sikandar <mdsikandar13@gmail.com>
2016-12-20 20:07:29 +0530

    Restrict actions from ui

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			session[:user_id] = user.id
-			flash[:success] =" You have successfully Login in"
+			flash[:success] = "You have successfully logged in"
 			redirect_to user_path(user)
 		else
-			flash.now[:danger] ="There was something wrong with your login information"
+			flash.now[:danger] = "There was something wrong with your login information"
 			render 'new'
 		end
 	end
